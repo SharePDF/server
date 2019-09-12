1a375e0be67d0c3061a18817967160e0e70904e9
saveroz <muhammadsavero@gmail.com>
2019-09-12 19:37:25 +0700

    added upload image

--- routes/PDF.js
+++ routes/PDF.js
@@ -1,10 +1,11 @@
 const Router = require('express').Router();
 const PDFController = require('../controllers/PDF');
 const Authenthication = require('../middleware/authenthication')
+const {sendUploadToGCS,multer} = require("../middleware/uploadPdf")
 
 Router.use(Authenthication)
 Router.get('/', PDFController.read)
-Router.post('/', PDFController.create)
+Router.post('/',multer.single("pdf"),sendUploadToGCS, PDFController.create)
 
 // ! Require Authorization
 Router.put('/:id', PDFController.update)
